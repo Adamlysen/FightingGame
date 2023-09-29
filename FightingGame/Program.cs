0a782d8e0d06dd0e602f33311c710399882a9ac2
Adiladis <adam.lysen@elev.ga.ntig.se>
2023-09-29 10:14:44 +0200

    dfdfdgsg

--- FightingGame/Program.cs
+++ FightingGame/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Security;
 
 namespace bajshora
 {
@@ -9,17 +10,17 @@ namespace bajshora
 
         public static void Main(string[] args)
         {
-            
+
 
             Console.WriteLine("Welcome!");
             Console.WriteLine("Do you want to play the FightingGame?");
 
             string play = Console.ReadLine();
 
-            while (play != "yes")
+            while (play.ToUpper() != "YES")
             {
 
-                if (play == "no")
+                if (play.ToUpper() == "YES")
                 {
 
                     return;
@@ -36,6 +37,7 @@ namespace bajshora
 
 
 
+
         }
 
 
@@ -44,7 +46,6 @@ namespace bajshora
         public static void Gameplay1()
         {
             Random generator = new Random();
-            
 
             Console.WriteLine("Enter Player 1 Name:");
             string name1 = Console.ReadLine();
@@ -54,12 +55,105 @@ namespace bajshora
             string name2 = Console.ReadLine();
 
             Console.WriteLine("Press enter to start the game");
-            Console.ReadLine();
-            Console.WriteLine("The fight begins");
-            Console.WriteLine($"{name1} and {name2} start engaging eachother");
+
             Console.ReadLine();
 
-            
+
+
+            Console.WriteLine("The fight begins");
+            Console.WriteLine($"{name1} and {name2} start engaging eachother");
+            Console.WriteLine();
+
+            int p1hp = 100;
+            int p2hp = 100;
+
+            Thread.Sleep(2500);
+
+
+
+
+            while (p1hp >= 0 && p2hp >= 0)
+            {
+
+
+                int p1damage = generator.Next(31);
+                int p2damage = generator.Next(31);
+
+                Console.WriteLine($"{name1}: {p1hp} hp");
+
+                Console.WriteLine($"{name2}: {p2hp} hp");
+
+                Thread.Sleep(2500);
+                Console.WriteLine();
+                Console.WriteLine($"{name1} deals {p1damage}");
+
+                p2hp = p2hp - p1damage;
+
+                if (p1damage <= 10)
+                {
+
+                    Console.WriteLine("How can you be that bad???");
+                    Console.WriteLine($"Only {p1damage}???");
+                    Console.WriteLine();
+                }
+                else if (p1damage > 10 && p1damage < 20)
+                {
+
+                    Console.WriteLine("Good Work!");
+                    Console.WriteLine();
+                }
+                else
+                {
+
+                    Console.WriteLine("A MAGNIFICENT ROPENIS ATTACK!!!");
+                    Console.WriteLine();
+                }
+
+                Thread.Sleep(2500);
+
+                Console.WriteLine($"{name2} deals {p2damage}");
+
+                p1hp = p1hp - p2damage;
+
+                if (p2damage <= 10)
+                {
+
+                    Console.WriteLine("How can you be that bad???");
+                    Console.WriteLine($"Only {p2damage}???");
+                    Console.WriteLine();
+                }
+                else if (p2damage > 10 && p2damage < 20)
+                {
+
+                    Console.WriteLine("Good Work!");
+                    Console.WriteLine();
+                }
+                else
+                {
+
+                    Console.WriteLine("A MAGNIFICENT ROPENIS ATTACK!!!");
+                    Console.WriteLine();
+                }
+
+                Thread.Sleep(2500);
+
+                if (p1hp > 0 && p2hp < 0)
+                {
+
+                    Console.WriteLine($"{name1} wins!");
+                    Console.WriteLine("Press enter to exit");
+
+                }
+                else if (p1hp < 0 && p2hp > 0)
+                {
+
+                    Console.WriteLine($"{name1} wins!");
+                    Console.WriteLine("Press enter to exit");
+
+                }
+
+
+            }
 
 
         }
